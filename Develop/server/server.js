2940155fbbcf6ee845cb0e872d3c1dc8f45f94f2
DEMETRIUSCARTER <108381693+DEMETRIUSCARTER@users.noreply.github.com>
2023-01-22 15:57:49 -0600

    Update server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-
 const { ApolloServer, gql } = require('apollo-server-express');
-
 const { authMiddleware } = require('./utils/auth');
 
 const schema = makeExecutableSchema({
