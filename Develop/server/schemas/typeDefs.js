9c475c5846c48c4541155d7b6ec65f1309244649
Demetrius Carter <demetriuscarter@gmail.com>
2023-01-22 21:00:51 -0600

    code update

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,26 @@
+const typeDefs = gql`
+    type Author {
+      id: Int!
+      first_name: String!
+      last_name: String!
+      books: [Book]!
+    }
+
+    type Book {
+      id: Int!
+      title: String!
+      cover_image_url: String!
+      average_rating: Float!
+      author: Author!
+    }
+
+    type Query {
+      books: [Book!]!,
+      book(id: Int!): Book!
+
+    }
+
+    type Mutation {
+      addBook(title: String!, cover_image_url: String!, average_rating: Float!, authorId: Int!): Book!
+    }
+  `;
