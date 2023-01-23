5cc7932b688d225ebeafc4a64e8ee82aff45aca9
Demetrius Carter <demetriuscarter@gmail.com>
2023-01-22 21:32:02 -0600

    code update

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer, gql } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
